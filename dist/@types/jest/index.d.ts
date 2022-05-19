0557472cc570258cdba410919605136560188931
Ryan Brandt <ryanbrandt@ryans-mbp.myfiosgateway.com>
2022-05-18 21:55:54 -0400

    Update install instructions
    bump version
    
    Rm dist
    
    ignore dist

--- dist/@types/jest/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="jest" />
-declare type AnyFunction = (...args: any[]) => any;
-declare class JestUtilities {
-    static assertAsMockFunction: <T extends AnyFunction>(actual: T) => jest.MockedFunction<T>;
-    static assertAsMockClass: <T extends jest.Constructable>(actual: T) => jest.MockedClass<T>;
-}
-export default JestUtilities;
